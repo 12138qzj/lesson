c60c4af81e7ed2b4ae203778ba31dd1e3bb18ac3
12138qzj <2442319903@qq.com>
2020-04-15 23:09:14 +0800

    20200415

--- es6/2.ts
+++ es6/2.ts
@@ -1,8 +1,8 @@
 
 
-const getArea  =(w :number , h :number/* 约束形参*/) : number/* 约束返回值 */ =>{
+const getAreas  =(w :number , h :number/* 约束形参*/) : number/* 约束返回值 */ =>{
 
     return w * h;
 }
 
-console.log(getArea(1,3));
+console.log(getAreas(1,3));
